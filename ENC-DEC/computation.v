fee3c13d919400857bfa29e0a0282f68d7c722e4
Kshitij Saxena <99387805+xytyz@users.noreply.github.com>
2023-04-06 17:32:58 +0530

    Update computation.v

--- ENC-DEC/computation.v
+++ ENC-DEC/computation.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
 
 module computation(
-    output[15:0] altered,
-	input [15:0] code);
-	wire[15:0]subbed;
-	subByte s1(.code(code),.altered(subbed));
-	shiftRow r1(.code(subbed),.altered(altered));	
-	
+	output[255:0] altered,
+	input [255:0] code);
+	wire[255:0]subbed,shifted;
+
+	subByte s1(.code(code),.altered(subbed)); //sub the bytes using LUTs
+	shiftRow r1(.code(subbed),.altered(shifted)); //Shift each row by a factor
+	mixCol m1(.code(shifted),.altered(altered)); //mix the columns
+
 endmodule
