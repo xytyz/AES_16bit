09797db3f1f267e220103c81190b97d487ab83be
Kshitij Saxena <99387805+xytyz@users.noreply.github.com>
2023-04-06 17:06:31 +0530

    Update rounds.v

--- ENC-DEC/rounds.v
+++ ENC-DEC/rounds.v
@@ -1,10 +1,12 @@
 module rounds(
-	output[15:0] altered,
-	input [15:0] code,
+	output[255:0] altered,
+	input [255:0] code,
 	input [3:0] count,
-	input [15:0] keyin,
-	output [15:0] keyout);
-	wire [15:0]computed;
+	input [255:0] keyin,
+	output [255:0] keyout
+);
+	
+	wire [255:0]computed;
 	
 	KeyGen k1(.count(count),.key(keyin),.keyout(keyout));
 	computation c1(.code(code),.altered(computed));	
