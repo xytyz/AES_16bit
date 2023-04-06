81051c308527057331da0e2043e1ec7bfb9d8b47
Kshitij Saxena <99387805+xytyz@users.noreply.github.com>
2023-04-06 17:40:00 +0530

    Update subByte.v

--- ENC-DEC/subByte.v
+++ ENC-DEC/subByte.v
@@ -4,11 +4,11 @@ module subByte(
 	input[15:0] code
     );
 	
-	wire [7:0] w1,w2;
-	assign w1 = code[7:0];
-	assign w2 = code[15:8];
-	
-	LUT_verilog s1(.outp(altered[7:0]),.inp(w1));
-	LUT_verilog s2(.outp(altered[15:8]),.inp(w2));
+	genvar i;
+	generate
+		for(i=0;i+7<=255;i+8) begin
+		LUT_verilog s1(.outp(altered[i+7:i]),.inp(code[i+7:i]));
+		end
+	endgenerate
 	
 endmodule
