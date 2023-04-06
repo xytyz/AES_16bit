cac745c0542929adad333bbb7a9d7f5e4370ca15
Kshitij Saxena <99387805+xytyz@users.noreply.github.com>
2023-04-06 17:00:50 +0530

    Update AES.v
    updated for 256 bits

--- ENC-DEC/AES.v
+++ ENC-DEC/AES.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 
 module AES(
-		output[15:0] outp, 
-		input[15:0] inpu, 
+		output[255:0] outp, 
+		input[255:0] inpu, 
 		input mode,
     );
-    wire [15:0] buff1,buff2;
+	wire [255:0] buff1,buff2;
     
-	encryption u1(.codein(inpu),.key(16'habcd),.codeout(buff1));
+	encryption u1(.codein(inpu),.key(256'habcd),.codeout(buff1));
 	
-	decryption u2(.codein(buff1),.key(16'habcd),.codeout(buff2),.prev(inpu));
+	decryption u2(.codein(buff1),.key(256'habcd),.codeout(buff2),.prev(inpu));
 	
 	assign outp= mode?buff1:buff2;
 	
